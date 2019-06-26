6157f5df399f85e727d3a1c84b3183219140a42f
Ranjith K Soman <ranjithks17@imperial.ac.uk>
2019-06-26 17:43:00 +0100

    added unity project

---
+++ 3D move/Assets/Behavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Display.displays.Length; i++)
+            Display.displays[i].Activate(1920,1080,60);
+    }
+
+    // Update is called once per frame
+   
+}
